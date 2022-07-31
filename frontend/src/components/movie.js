80bf9d7ac5efc3bc4715217d66891003c3c0be9e
Joshua <buildingguy@sbcglobal.net>
2022-07-31 12:55:31 -0700

    simple boilerplate for each component

--- frontend/src/components/movie.js
+++ frontend/src/components/movie.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-function Login() {
+function Movie() {
     return (
         <div className='App'>
-            Login
+            Movie
         </div>
     );
 }
 
-export default Login;
+export default Movie;
