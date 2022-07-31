e7b1ca3f890972566a515676db7e576299ee0e91
Joshua <buildingguy@sbcglobal.net>
2022-07-31 13:30:58 -0700

    create navigation header bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,11 +8,48 @@ import AddReview from './components/add-review';
 import MoviesList from './components/movies-list';
 import Movie from './components/movie';
 import Login from './components/login';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
 
 function App() {
+  // React.useState is a hook that allows for the addition of local state to functional component
+  // useState returns an array with two values an a function that can be updated
+  const [user, setUser] = React.useState(null);
+
+  // set user state
+  async function login(user = null) { // default user to null
+    setUser(user);
+  }
+
+  // set user to null
+  async function logout() {
+    setUser(null)
+  }
+
   return (
     <div className="App">
-      Hello World
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand>Movie Reviews</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link>
+              {/* Link allows routing to different components */}
+              <Link to={"/movie"}>Movies</Link>
+            </Nav.Link>
+            <Nav.Link>
+              { user ? (
+                  // display if user is logged in
+                  <a onClick={logout}>Logout User</a>
+                ) : (
+                  // display if user is logged out
+                  <Link to={"/login"}>Login</Link>
+              )}
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+    </Navbar>
     </div>
   );
 }
