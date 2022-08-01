01d9f4b56c625e5410903da6c066a44087d04ab9
Joshua <buildingguy@sbcglobal.net>
2022-08-01 16:12:13 -0700

    create movie component

--- frontend/src/components/movie.js
+++ frontend/src/components/movie.js
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import MovieDataService from '../service/movies';
+import { Link } from 'react-router-dom';
 
-function Movie() {
+const Movie = props => {
+
+    // hold specific movie being shown
+    const [movie, setMovie] = useState({
+        id: '',
+        title: "",
+        rated: "",
+        reviews:[]
+    });
+
+    // call MovieDataService.get()
+    const getMovie = id => {
+        MovieDataService.get(id)
+            .then(response => {
+                setMovie(response.data);
+                console.log(response.data);
+            })
+            .catch(e => {
+                console.log(e);
+            })
+    }
+
+    // call getMovie
+    useEffect(() => {
+        getMovie(props.match.params.id)
+    }, [props.match.params.id] /* avoid calling multiple times for same movie*/)
+
     return (
-        <div className='App'>
-            Movie
+        <div>
         </div>
     );
 }
+
+
+// function Movie() {
+//     return (
+//         <div className='App'>
+//             Movie
+//         </div>
+//     );
+// }
 
 export default Movie;
