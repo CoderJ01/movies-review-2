081acfd5ea4f9eacfed1c8aa47de33b07b1ebaae
Joshua <buildingguy@sbcglobal.net>
2022-08-01 14:30:29 -0700

    create useState variables for movie list component

--- frontend/src/components/movies-list.js
+++ frontend/src/components/movies-list.js
@@ -1,11 +1,24 @@
-import React from 'react';
+// useState allows for the the creation of several variables
+import React, {useState, useEffect} from 'react';
+import MovieDataService from "../services/movies";
+import { Link } from "react-router-dom";
 
-function MoviesList() {
-    return (
-        <div className='App'>
-            Movies List
-        </div>
-    );
+// recieves and uses props
+const MoviesList = props => {
+    // useState hooks create state variables (blue)
+    // 2nd and 3rd lines keep track of what user has entered
+    const [movies, setMovies] = useState([]);
+    const [searchTitle, setSearchTitle] = useState("");  
+    const [searchRatings, setSearchRatings] = useState("");
+    const [ratings, setRatings] = useState(["All Ratings"]);
 }
+
+// function MoviesList() {
+//     return (
+//         <div className='App'>
+//             Movies List
+//         </div>
+//     );
+// }
 
 export default MoviesList;
