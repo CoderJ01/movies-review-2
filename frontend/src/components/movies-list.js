7291a9667e8521ef757743490d49145547cd1ca3
Joshua <buildingguy@sbcglobal.net>
2022-08-01 14:40:16 -0700

    retrieve movies and ratings

--- frontend/src/components/movies-list.js
+++ frontend/src/components/movies-list.js
@@ -11,6 +11,37 @@ const MoviesList = props => {
     const [searchTitle, setSearchTitle] = useState("");  
     const [searchRatings, setSearchRatings] = useState("");
     const [ratings, setRatings] = useState(["All Ratings"]);
+
+    // performed after rendering
+    useEffect(() => {
+        retrieveMovies()
+        retrieveRatings()
+    },[])
+
+    // retrieve movies
+    const retrieveMovies = () => {
+        MovieDataService.getAll()
+            .then(response => {
+                console.log(response.data)
+                setMovies(response.data.movies)
+            })
+            .catch(e => {
+                console.log(e);
+            })
+    }
+
+    // retrieve ratings
+    const retrieveRatings = () => {
+        MovieDataService.getRatings()
+        .then(response => {
+            console.log(response.data);
+            // start with 'All ratings' if user doesn't specify any ratings
+            setRatings(["All Ratings"].concat(response.data))
+            .catch(e => {
+                console.log(e);
+            });
+        })
+    }
 }
 
 // function MoviesList() {
