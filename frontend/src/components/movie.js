b5ae083d48d066571bbfb0bcade411e18e2c662d
Joshua <buildingguy@sbcglobal.net>
2022-08-01 17:01:19 -0700

    movie component markup

--- frontend/src/components/movie.js
+++ frontend/src/components/movie.js
@@ -1,6 +1,13 @@
 import React, {useState, useEffect} from 'react';
 import MovieDataService from '../service/movies';
 import { Link } from 'react-router-dom';
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap';
+import Image from 'react-bootstrap/Image';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
+import Media from 'react-bootstrap/Media';
 
 const Movie = props => {
 
@@ -31,6 +38,29 @@ const Movie = props => {
 
     return (
         <div>
+            <Container>
+                <Row>
+                    {/* movie poster */}
+                    <Col>
+                        <Image src={movie.poster+ "/100px250"} fluid />
+                    </Col>
+                    {/* movie details */}
+                    <Col>
+                        <Card>
+                            <Card.Header as="h5">{movie.title}</Card.Header>
+                            <Card.Body>
+                                <Card.Text>
+                                    {movie.plot}
+                                </Card.Text>
+                                {props.user && 
+                                <Link to={"/movies/" + props.match.params.id + "/review"}>
+                                    Add Review
+                                </Link>}
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
