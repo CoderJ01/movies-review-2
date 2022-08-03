6607569430664ae2d71b1b221df7fe973d6973bd
Joshua <buildingguy@sbcglobal.net>
2022-08-03 15:21:39 -0700

    use Heroku generated URL instead of localhost

--- frontend/src/services/movies.js
+++ frontend/src/services/movies.js
@@ -4,39 +4,39 @@ class MovieDataService {
 
     // return movies for a particular page
     getAll(page = 0) {
-        return axios.get(`https://localhost:5000/api/v1/movies?page=${page}`);
+        return axios.get(`https://mern-movie-reviews.herokuapp.com/api/v1/movies?page=${page}`);
     }
 
     // get a movie with a specific id
     get(id) {
-        return axios.get(`https://localhost:5000/api/v1/movies/id/${id}`);
+        return axios.get(`https://mern-movie-reviews.herokuapp.com/api/v1/movies/id/${id}`);
     }
 
     // connects to the same endpoint as getAll, except it has a query which consists
     // of user-entered search title, ratings, and page number
     find(query, by = 'title', page='0') {
         return axios.get(
-            `https://localhost:5000/api/v1/movies?${by}=${query}&page=${page}`
+            `https://mern-movie-reviews.herokuapp.com/api/v1/movies?${by}=${query}&page=${page}`
         );
     }
 
     createReview(data) {
-        return axios.post("https://localhost:5000/api/v1/movies/review", data);
+        return axios.post("https://mern-movie-reviews.herokuapp.com/api/v1/movies/review", data);
     }
 
     updateReview(data) {
-        return axios.put("https://localhost:5000/api/v1/movies/review", data);
+        return axios.put("https://mern-movie-reviews.herokuapp.com/api/v1/movies/review", data);
     }
 
     deleteReview(id, userId) {
         return axios.delete(
-            "https://localhost:5000/api/v1/movies/review",
+            "https://mern-movie-reviews.herokuapp.com/api/v1/movies/review",
             {data:{review_id: id, user_id: userId}}
         );
     }
 
     getRatings() {
-        return axios.get("https://localhost:5000/api/v1/movies/ratings");
+        return axios.get("https://mern-movie-reviews.herokuapp.com/api/v1/movies/ratings");
     }
 }
 
